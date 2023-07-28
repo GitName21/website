6f2c18a06cb4d80d44909767c604b9a80c96e5c2
GitName21 <915519469@qq.com>
2023-07-28 09:02:06 +0800

    新增时间jq

--- js/jquery.js
+++ js/jquery.js
@@ -401,6 +401,21 @@ $(document).ready(function(){
 			$(".content-menu-item li").eq(liEq).addClass("current-menu-item");
 		}
 		console.log(liEq)
-	});	
+	});
 	
+	// 时间获取
+	    var myDate = new Date;
+	    var year = myDate.getFullYear(); //获取当前年
+	    var mon = myDate.getMonth() + 1; //获取当前月
+	    var date = myDate.getDate(); //获取当前日
+	    var h = myDate.getHours();//获取当前小时数(0-23)
+	    var m = myDate.getMinutes();//获取当前分钟数(0-59)
+	    var s = myDate.getSeconds();//获取当前秒
+	    var week = myDate.getDay();
+	    var weeks = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"];
+	    console.log(year, mon, date, weeks[week])
+	    $(".week").html(weeks[week]);
+	    $(".date").html(mon + '月' + date + ',' + year);
+	    $(".time span:first-child").html(h + '点' + m + '分');
+
 });
